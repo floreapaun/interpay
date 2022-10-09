34fd5c39da027411f4c155f86d959462bf802d39
serj99 <go_with_the_flow99@yahoo.com>
2022-10-09 13:38:01 +0300

    Use prepared statements.

--- insert.php
+++ insert.php
@@ -1,43 +1,45 @@
 <?php
-    $host        = "host = 127.0.0.1";
-    $port        = "port = 5432";
-    $dbname      = "dbname = interpay";
-    $credentials = "user = postgres password=4137";
 
-    $db = pg_connect( "$host $port $dbname $credentials"  );
-    if(!$db) {
-        echo "Error: Unable to open database<br>";
-    } else {
-        echo "Opened database successfully<br>";
+    $dsn = "pgsql:dbname=interpay host=localhost";
+    $options = [
+      PDO::ATTR_EMULATE_PREPARES   => false, 
+      PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, 
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, 
+    ];
+    try {
+      $pdo = new PDO($dsn, "postgres", "4137", $options);
+    } catch (Exception $e) {
+      error_log($e->getMessage());
+      exit('Fatal error!');
     }
-        
-    $sql1 =<<<EOF
+
+    function tableExists($pdo, $table) {
+        $table = preg_replace('/[^\da-z_]/i', '', $table);
+        try {
+            $result = $pdo->query("SELECT 1 FROM {$table} LIMIT 1");
+        } catch (Exception $e) {
+            return FALSE;
+        }
+        return $result !== FALSE;
+    }
+
+    $sql = <<<EOF
         CREATE TABLE authors
         (id SERIAL PRIMARY KEY,
         name TEXT NOT NULL);
     EOF;
+    if (!tableExists($pdo, "authors"))
+        $pdo->prepare($sql)->execute();
 
-    $sql2 =<<<EOF
+    $sql = <<<EOF
         CREATE TABLE books
         (id SERIAL PRIMARY KEY,
         author_id INT NOT NULL,
         name TEXT NOT NULL,
         CONSTRAINT fk_author FOREIGN KEY(author_id) REFERENCES authors(id));
     EOF;
-
-    $ret = pg_query($db, $sql1);
-    if(!$ret) {
-        echo pg_last_error($db) . "<br>";
-    } else {
-        echo "Authors table created successfully<br>";
-    }
-
-    $ret = pg_query($db, $sql2);
-    if(!$ret) {
-        echo pg_last_error($db) . "<br>";
-    } else {
-        echo "Books table created successfully<br>";
-    }
+    if (!tableExists($pdo, "books"))
+        $pdo->prepare($sql)->execute();
 
     //XML file path
     $path = "books.xml";
@@ -56,69 +58,57 @@
 
     for($i = 0; $i < count($booksArr['book']); $i++) {
 
-        //Check if author inserted into table
+        //Check if author already inserted into table
         $sql = <<<EOF
-            SELECT * FROM authors WHERE name = '{$booksArr['book'][$i]['author']}';
+            SELECT * FROM authors WHERE name = ?;
         EOF;
 
-        $ret = pg_query($db, $sql);
-        if (!$ret) {
-           echo pg_last_error($db);
-        } 
-        $author_row = pg_fetch_row($ret);
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(1, $booksArr['book'][$i]['author'], PDO::PARAM_STR);
+        $stmt->execute();
+        $author_row = $stmt->fetch();
 
-        //If author does not exist then insert into table
-        if (!$author_row) {
-            $sql = <<<EOF
-                INSERT INTO authors (name)
-                VALUES ('{$booksArr['book'][$i]['author']}');
-            EOF;
-
-            $ret = pg_query($db, $sql);
-            if (!$ret) {
-                echo pg_last_error($db);
-            } else {
-                echo "Author record created successfully<br>";
-            }
-        }
-
-        //If author was not inserted before get the last inserted author
-        $sql = "SELECT * FROM authors ORDER BY id DESC LIMIT 1";
-        $ret = pg_query($db, $sql);
-        if(!$ret) {
-            echo pg_last_error($db);
-        }
-        $row = pg_fetch_row($ret);
-        $last_author_id = $row[0];
-
-        //If author was inserted before
         if ($author_row)
-            if ($author_row[0])
-                $last_author_id = $author_row[0];
+            $last_author_id = $author_row['id'];
+        else {
+            $sql = <<<EOF
+                INSERT INTO authors (name) VALUES (?);
+            EOF;
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(1, $booksArr['book'][$i]['author'], PDO::PARAM_STR);
+            $stmt->execute();
+            echo "Author record created successfully!<br>";
+
+            $sql = "SELECT * FROM authors ORDER BY id DESC LIMIT 1";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute();
+            $author_row = $stmt->fetch();
+            $last_author_id = $author_row['id'];
+        }
         
         //Check if book inserted into table
         $sql = <<<EOF
-            SELECT * FROM books WHERE name = '{$booksArr['book'][$i]['name']}';
+            SELECT * FROM books WHERE name = ?;
         EOF;
-        $ret = pg_query($db, $sql);
-        if (!$ret) {
-            echo pg_last_error($db);
-        } 
-        $row = pg_fetch_row($ret);
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(1, $booksArr['book'][$i]['name'], PDO::PARAM_STR);
+        $stmt->execute();
+        $book_row = $stmt->fetch();
 
         //If book does not exist then insert into table
-        if (!$row) {
+        if (!$book_row) {
             $sql = <<<EOF
-                INSERT INTO books (author_id, name)
-                VALUES ({$last_author_id}, '{$booksArr['book'][$i]['name']}');
+                INSERT INTO books (author_id, name) VALUES (?, ?);
             EOF;
-
-            $ret = pg_query($db, $sql);
-            if(!$ret) {
-                echo pg_last_error($db);
-            } else {
-                echo "Book record created successfully<br>";
-            }
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(1, $last_author_id, PDO::PARAM_INT);
+            $stmt->bindParam(2, $booksArr['book'][$i]['name'], PDO::PARAM_STR);
+            $stmt->execute();
+            echo "Book record created successfully!<br>";
         }
     }
+
+    $pdo = null;
+    $stmt = null;
+
 ?>
--- search/search.php
+++ search/search.php
@@ -1,50 +1,46 @@
 <?php
 
-    $host        = "host = 127.0.0.1";
-    $port        = "port = 5432";
-    $dbname      = "dbname = interpay";
-    $credentials = "user = postgres password=4137";
-
-    $db = pg_connect( "$host $port $dbname $credentials"  );
-    if(!$db) {
-        echo "Error : Unable to open database<br>";
-    } 
-
-	//check whether user types a search string and processing it
-	if(isset($_POST["query"]))
-	{
-		$sql="SELECT authors.name AS aname, books.name AS bname FROM authors 
-            INNER JOIN books ON books.author_id = authors.id 
-            WHERE authors.name LIKE '%" . $_POST["query"] . "%'";
-
-        $ret = pg_query($db, $sql);
-
-        if($ret)
-        {
-            $output = '';
-            $output .= "<table id='response_table'>";
-            if(pg_num_rows($ret))
-            {
-                while($row = pg_fetch_array($ret)) 
-                {
-                    $output .= "<tr>";
-                    $output .= "<td>" . $row['aname'] . '</td>';
-                    $output .= "<td>" . $row['bname'] . '</td>';
-                    $output .= "</tr>";
-                }
-                $output .= "</table>";
-                echo $output;
-    
-            }
-            else
-            {
-                echo 'No Data For This Id';
-            }
-        }
-        else
-        {
-            echo 'Result Error';
-        }
+    $dsn = "pgsql:dbname=interpay host=localhost";
+    $options = [
+        PDO::ATTR_EMULATE_PREPARES   => false, 
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, 
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, 
+    ];
+    try {
+        $pdo = new PDO($dsn, "postgres", "4137", $options);
+    } catch (Exception $e) {
+        error_log($e->getMessage());
+        exit('Fatal error!');
     }
+
+	if (isset($_POST["query"])) {
+		$sql = <<<EOF
+            SELECT authors.name AS aname, books.name AS bname FROM authors 
+            INNER JOIN books ON books.author_id = authors.id 
+            WHERE authors.name LIKE ?;
+        EOF;
+
+        $name = "%{$_POST['query']}%";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(1, $name, PDO::PARAM_STR);
+        $stmt->execute();
+        
+        $output = "<table id='response_table'>";
+        while ($row = $stmt->fetch()) {
+            $output .= "<tr>";
+            $output .= "<td>" . $row['aname'] . '</td>';
+            $output .= "<td>" . $row['bname'] . '</td>';
+            $output .= "</tr>";
+        }
+        if (strlen($output) < 28)
+            echo "No authors found.";
+        else {
+            $output .= "</table>";
+            echo $output;
+        }          
+    }
+
+    $pdo = null;
+    $stmt = null;
 	
 ?>
